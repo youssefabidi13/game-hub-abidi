85ea2bbae7395972a404dccaf7b36860f53c25b0
youssefabidi13 <youssef.abidi@etu.uae.ac.ma>
2024-02-13 19:18:59 +0100

    Refactor:remove duplicated style

---
+++ src/components/GameCardContainer.tsx
@@ -0,0 +1,14 @@
+import { Box } from "@chakra-ui/react";
+import React from "react";
+interface Props {
+  children: React.ReactNode;
+}
+const GameCardContainer = ({ children }: Props) => {
+  return (
+    <Box width="250px" borderRadius={10} overflow="hidden">
+      {children}
+    </Box>
+  );
+};
+
+export default GameCardContainer;
