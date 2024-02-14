e91821e1cf764515b42c8bb44028624847d9f393
youssefabidi13 <youssef.abidi@etu.uae.ac.ma>
2024-02-14 17:26:21 +0100

    Display Genres

--- src/App.tsx
+++ src/App.tsx
@@ -14,12 +14,16 @@ function App() {
         "nav nav"
         "aside main"`, //1024px
       }}
+      templateColumns={{
+        base: "1fr",
+        lg: "200px 1fr",
+      }}
     >
       <GridItem area="nav">
         <NavBar />
       </GridItem>
       <Show above="lg">
-        <GridItem area="aside">
+        <GridItem area="aside" paddingX={5}>
           <GenreList />
         </GridItem>
       </Show>
